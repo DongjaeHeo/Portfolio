824698b3c4490b323deacdeb08a8a5cba3a4b252
Dongjae Heo <lindsay.heo@gmail.com>
2023-05-19 16:43:54 +0100

    adjust size of font

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ window.addEventListener("resize", (event) => {
 const opacityChange = (scroll, windowHeight) => {
   if (scroll <= 4 * windowHeight && scroll > 3 * windowHeight) {
     const op = (scroll - 3 * windowHeight) / windowHeight;
-    document.querySelector("#main").style.opacity = 0.7 + op;
+    document.querySelector("#main").style.opacity = 0.85 + op;
     document.querySelector("#main-1").style.opacity = 0.5 + op;
   }
   if (scroll <= 4 * windowHeight && scroll > 2 * windowHeight) {
     if (scroll >= 3 * windowHeight) {
       const op = (4 * windowHeight - scroll) / windowHeight;
-      document.querySelector("#skill").style.opacity = 0.7 + op;
+      document.querySelector("#skill").style.opacity = 0.85 + op;
       document.querySelector("#skill-1").style.opacity = 0.5 + op;
     } else {
       const op = (scroll - 2 * windowHeight) / windowHeight;
-      document.querySelector("#skill").style.opacity = 0.7 + op;
+      document.querySelector("#skill").style.opacity = 0.85 + op;
       document.querySelector("#skill-1").style.opacity = 0.5 + op;
     }
   }
   if (scroll <= 3 * windowHeight && scroll > 1 * windowHeight) {
     if (scroll >= 2 * windowHeight) {
       const op = (3 * windowHeight - scroll) / windowHeight;
-      document.querySelector("#work").style.opacity = 0.7 + op;
+      document.querySelector("#work").style.opacity = 0.85 + op;
       document.querySelector("#work-1").style.opacity = 0.5 + op;
     } else {
       const op = (scroll - 1 * windowHeight) / windowHeight;
-      document.querySelector("#work").style.opacity = 0.7 + op;
+      document.querySelector("#work").style.opacity = 0.85 + op;
       document.querySelector("#work-1").style.opacity = 0.5 + op;
     }
   }
   if (scroll <= 2 * windowHeight) {
     const op = (2 * windowHeight - scroll) / windowHeight;
-    document.querySelector("#contact").style.opacity = 0.7 + op;
+    document.querySelector("#contact").style.opacity = 0.85 + op;
     document.querySelector("#contact-1").style.opacity = 0.5 + op;
   }
 };
